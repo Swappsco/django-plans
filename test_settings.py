d3a5f214ceef7539b4a675eb15d152d208d38af3
cdvv7788 <cristian@swapps.co>
2015-11-17 13:27:09 -0500

    PLANS-1 Add files to run tests for plan directly, instead of running it from the demo app

---
+++ test_settings.py
@@ -0,0 +1,109 @@
+# Django settings for example project.
+import os
+from decimal import Decimal
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': ':memory:',  # Or path to database file if using sqlite3.
+        'USER': '',  # Not used with sqlite3.
+        'PASSWORD': '',  # Not used with sqlite3.
+        'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',  # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+TIME_ZONE = 'America/Chicago'
+USE_TZ = True
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = True
+USE_L10N = True
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+STATIC_ROOT = ''
+STATIC_URL = '/static/'
+
+
+SECRET_KEY = 'l#^#iad$8$4=dlh74$!xs=3g4jb(&j+y6*ozy&8k1-&d+vruzy'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    #     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'plans.urls'
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'ordered_model',
+    'plans',
+
+)
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'DIRS': [],
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+                # django-plans context processor
+                'plans.context_processors.account_status',
+            ],
+        },
+    },
+]
+
+
+LANGUAGES = (
+    ('en', 'English'),
+)
+
+PLANS_INVOICE_ISSUER = {
+    "issuer_name": "My Company Ltd",
+    "issuer_street": "48th Suny street",
+    "issuer_zipcode": "111-456",
+    "issuer_city": "Django City",
+    "issuer_country": "PL",
+    "issuer_tax_number": "PL123456789",
+}
+
+PLANS_TAX = Decimal('23.0')
+PLANS_TAXATION_POLICY = 'plans.taxation.eu.EUTaxationPolicy'
+PLANS_TAX_COUNTRY = 'PL'
+
+PLANS_VALIDATORS = {
+    'MAX_FOO_COUNT': 'example.foo.validators.max_foos_validator',
+}
+
+PLANS_CURRENCY = 'EUR'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_REDIRECT_URL = '/foo/list/'
