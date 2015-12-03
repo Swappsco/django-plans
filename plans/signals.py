7653bc7a9601717e912d2f73dd24203034feebcf
Sebastian Reyes Espinosa <sebaslander@gmail.com>
2015-12-03 08:48:13 -0500

    [PLANS] Updating the quota and signals file code to be more Python3 oriented, but also keeping it compatible with Python2, with a nice touch of PEPS8

--- plans/signals.py
+++ plans/signals.py
@@ -12,18 +12,22 @@ Sent after order was completed (payment accepted, account extended)
 
 
 user_language = Signal(providing_args=['user', 'language'])
-user_language.__doc__ = """Sent to receive information about language for user account"""
+user_language.__doc__ = """
+Sent to receive information about language for user account"""
 
 
 
 account_expired = Signal(providing_args=['user'])
 account_expired.__doc__ = """
-Sent on account expiration. This signal is send regardless ``account_deactivated`` it only means that account has expired due to plan expire date limit.
+Sent on account expiration. This signal is send regardless
+``account_deactivated`` it only means that account has expired due to plan
+expire date limit.
 """
 
 account_deactivated = Signal(providing_args=['user'])
 account_deactivated.__doc__ = """
-Sent on account deactivation, account is not operational (it could be not expired, but does not meet quota limits).
+Sent on account deactivation, account is not operational (it could be not
+expired, but does not meet quota limits).
 """
 
 account_activated = Signal(providing_args=['user'])
@@ -37,5 +41,7 @@ Sent on account when plan was changed after order completion
 
 activate_user_plan = Signal(providing_args=['user'])
 activate_user_plan.__doc__ = """
-This signal should be called when user has succesfully registered (e.g. he activated account via e-mail activation). If you are using django-registration there is no need to call this signal.
+This signal should be called when user has succesfully registered (e.g. he
+activated account via e-mail activation). If you are using django-registration 
+there is no need to call this signal.
 """
