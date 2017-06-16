837e01b00c7e248e2b1b5a86a1cb784ee3f4f94a
Jakub Kuszneruk <jakub.kuszneruk@rdlabs.pl>
2017-06-16 22:14:14 +0200

    [#6] Configured time before expiration alert appears
    - Now time when expiration warning will appear is configured. It's the biggest value from settings.PLANS_EXPIRATION_REMIND or 14.
    piggyback:
    - removed deprecated functions and configurations from demo urls and settings

--- demo/example/urls.py
+++ demo/example/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls import url, patterns, include
+from django.contrib.auth import views as auth_views
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^plan/', include('plans.urls')),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="login"),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name="logout"),
+    url(r'^accounts/login/$', auth_views.login, name="login"),
+    url(r'^accounts/logout/$', auth_views.logout, {'next_page': '/'},
+        name="logout"),
     url(r'^foo/', include('example.foo.urls')),
-)
+]
